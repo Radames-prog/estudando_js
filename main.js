4d8b9e60985e9f6dcfecc83220bdb58cf6ef3d01
Radames-prog <easycompvila@gmail.com>
2021-10-23 10:12:17 -0300

    Array

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("Meu primeiro js")
+var lista = ["maçã","pera","laraja"]
+lista.push("uva")
+
+console.log(lista[1])
+console.log(lista)
+//var nome = 'Radames'
+//var idade = 29
+//var idade2 = 20
+
+//alert('Bem vindo' + nome)
+
+//alert(idade + idade2)
